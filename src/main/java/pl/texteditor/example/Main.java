f808c9f3cef94c2327aacf612d58a391ee2559f3
Sergiusz <deephans@o2.pl>
2018-04-09 18:22:56 +0200

    added example view for test purposes

---
+++ src/main/java/pl/texteditor/example/Main.java
@@ -0,0 +1,25 @@
+package pl.texteditor.example;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setTitle("Hello World!");
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/example/Example.fxml"));
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setMaximized(true);
+        primaryStage.show();
+    }
+}
