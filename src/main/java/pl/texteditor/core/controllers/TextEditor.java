68a8046816ae60b9d25d6ab26a8aa2a576323b29
Sergiusz <deephans@o2.pl>
2018-04-11 19:37:01 +0200

    added some mock text editor view (for tests)

--- src/main/java/pl/texteditor/core/controllers/TextEditor.java
+++ src/main/java/pl/texteditor/core/controllers/TextEditor.java
@@ -1,11 +1,26 @@
 package pl.texteditor.core.controllers;
 
+import com.sun.javafx.collections.ObservableListWrapper;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import pl.texteditor.core.TextEditorCellFactory;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class TextEditor extends AnchorPane {
+public class TextEditor extends AnchorPane implements Initializable {
+
+    @FXML
+    private ListView<TextFlow> textEditorListView;
+
     public TextEditor() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/editor/TextEditor.fxml"));
         fxmlLoader.setRoot(this);
@@ -16,5 +31,19 @@ public class TextEditor extends AnchorPane {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        textEditorListView.setCellFactory(new TextEditorCellFactory());
+        mockListView();
+    }
+
+    private void mockListView() {
+        List<TextFlow> lines = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            lines.add(new TextFlow(new Text("sadsasasdasd")));
+        }
+        textEditorListView.setItems(new ObservableListWrapper<>(lines));
     }
 }
